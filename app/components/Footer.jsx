e7e60934a918ad3112d779942050d297f085a114
tareqferdous <16101017@uap-bd.edu>
2025-06-11 07:38:01 +0600

    added footer to portfolio website

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { assets } from "@/assets/assets";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-20">
+      <div className="text-center">
+        <Image src={assets.logo} alt="" className="w-36 mx-auto mb-2" />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image src={assets.mail_icon} alt="" className="w-6" />
+          tarekferdos@gmail.com
+        </div>
+      </div>
 
-export default Footer
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>@ 2025 Tareq Ferdous. All rights reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="github.com/tareqferdous">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="linkedin.com/in/tareqferdous/">
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://x.com/TareqFerdous17">
+              X(Twitter)
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
