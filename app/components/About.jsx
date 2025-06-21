70d70e6ce21b73966272711986caa8949e69fee2
tareqferdous <16101017@uap-bd.edu>
2025-06-21 20:28:37 +0600

    update about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -53,7 +53,7 @@ const About = ({ isDarkMode }) => {
           className="flex-1"
         >
           <p className="mb-10 max-w-2xl font-Ovo">
-            I am an experienced Frontend Developer with over a decade of
+            I am an Digital marketing expert with over a decade of
             professional expertise in the field. Throughout my career, I have
             had the privilege of collaborating with prestigious organizations,
             contributing to their success and growth.
